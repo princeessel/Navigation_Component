1dc9c8ee2f007dbc88458af9892a5878b74c0d83
princeessel <43561228+princeessel@users.noreply.github.com>
2019-10-08 21:28:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/navigationcomponent/ConfirmationFragment.kt
@@ -0,0 +1,38 @@
+package com.example.navigationcomponent
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class ConfirmationFragment : Fragment() {
+    lateinit var money: Money
+    lateinit var recipient: String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        recipient = arguments!!.getString("recipient")
+        money = arguments!!.getParcelable("amount")
+
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val amount = money!!.amount
+        val confirmationMessage = "You have sent $amount to $recipient"
+        view.findViewById<TextView>(R.id.confirmation_message_tv).text = confirmationMessage
+    }
+}
---
+++ app/src/main/java/com/example/navigationcomponent/MainFragment.kt
@@ -0,0 +1,44 @@
+package com.example.navigationcomponent
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class MainFragment : Fragment(), View.OnClickListener {
+    var navController: NavController? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.view_transactions_button).setOnClickListener(this)
+        view.findViewById<Button>(R.id.view_balance_button).setOnClickListener(this)
+        view.findViewById<Button>(R.id.send_money_button).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+when(v!!.id){
+    R.id.view_transactions_button -> navController!!.navigate(R.id.action_fragmentMain_to_viewTransactionFragment2)
+    R.id.view_balance_button -> navController!!.navigate(R.id.action_fragmentMain_to_fragmentViewBalance2)
+    R.id.send_money_button -> navController!!.navigate(R.id.action_fragmentMain_to_fragmentChooseRecipient2)
+}
+    }
+
+
+}
